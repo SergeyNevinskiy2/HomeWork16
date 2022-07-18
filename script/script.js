a38441cd634ce0c7a6999e4add67596a62a00e5d
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-18 19:04:04 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,21 @@
 'use strict';
 
 const userName = +prompt('Введите число');
+let minDiv = 0;
 if(userName < 1) {
     console.log('NaN')
-}else if (userName === 1){
-    console.log('1 не простое число')
-}
-
-if (userName > 1) {
+}else{
     for(let i = 2; i < userName; i++){
         if(userName % i === 0){
-            console.log(`Минимальный делитель твоего числа ${i}`)
-        break
-        }else{
-            console.log(`Число простое и минимальный делитель ${userName} и 1`);
+            minDiv = i;
+            console.log(`Минимальный делитель твоего числа ${minDiv}`)
             break;
-        }
-    }
+}
+}
+
+
+if (minDiv === 0) {
+    console.log(`Число простое и минимальный делитель ${userName} и 1`)
+        
+}
 }
